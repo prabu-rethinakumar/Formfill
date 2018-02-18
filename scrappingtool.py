45f88bb262624c5be36dd5f1a6806ab6a68dd819
Prabu Rethinakumar <prabu.rethinakumar@gmail.com>
2018-02-17 20:07:47 -0500

    Finished scrapping

--- scrappingtool.py
+++ scrappingtool.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from time import sleep
-# from selenium.webdriver.common import keys,action_chains
+import json
+# from selenium.webdriver.common import alert
 # from selenium.webdriver.chrome.webdriver import Options
 import sys
 from selenium.common.exceptions import *
@@ -12,7 +13,8 @@ url = "https://www.glassdoor.com/index.htm"
 def get_total_pages(browser):
     total = browser.find_element_by_id("TotalPages").get_attribute("value")
     print("Search Results span across {} pages ".format(total))
-    return total
+    return int(total)
+
 
 def web_browser():
     chrome_options = webdriver.ChromeOptions()
@@ -32,6 +34,16 @@ def web_browser():
     return browser1
 
 
+def get_jobs_from_page(output_list, browser):
+    listings = browser.find_elements_by_class_name("jobLink")
+
+    for i in listings:
+        hyper_link = i.get_attribute('href')
+        job_list_id = hyper_link[len(hyper_link)-10:]
+        output_list.setdefault(job_list_id, hyper_link)
+    return output_list
+
+
 def get_job_details(website, job_name):
     print("Initialize Browser")
     browser = web_browser()
@@ -44,6 +56,7 @@ def get_job_details(website, job_name):
         form_job_data.send_keys(job_name)
         form_job_location = browser.find_element_by_id("LocationSearch")
         form_job_location.clear()
+        sleep(2)
         form_submit = browser.find_element_by_id("HeroSearchButton")
         form_submit.click()
         print("Job Search Initialized Successfully")
@@ -53,29 +66,54 @@ def get_job_details(website, job_name):
         print("Failed to Click Search button")
         sys.exit("Error Message")
 
-    sleep(2)
-    iter = get_total_pages(browser)
+    sleep(5)
+    itern = get_total_pages(browser)
 
-#   save_url = browser.current_url
-#   print("Save url contains :{}".format(save_url))
-#   sleep(2)
-#   job_list = browser.find_element_by_class_name("jl")
-#   job_link_list = browser.find_elements_by_partial_link_text("/partner/jobListing.htm?")
     print("Started collecting Job listings")
-
-    job_link_list = browser.find_elements_by_class_name("jobLink")
-    sleep(2)
     j = {}
-    for i in job_link_list:
-        hyper_link = i.get_attribute('href')
-        job_list_id = hyper_link[len(hyper_link)-10:]
-        j.setdefault(job_list_id, hyper_link)
-#       print("Job Listing : {} URL : {}".format(job_list_id, hyper_link))
+    for i in range(1, itern + 1, 1):
+        print("Working on Page {} ".format(i))
+
+        try:
+            browser.find_element_by_class_name("mfp-close").click()
+        except NoSuchElementException:
+            pass
+
+        get_jobs_from_page(j, browser)
+
+        try:
+            browser.find_element_by_class_name("next").click()
+            print("Clicked next page")
+            sleep(5)
+
+        except NoSuchElementException:
+            print("Reached Last page in search..exiting")
+        except WebDriverException:
+            sleep(2)
+            print("Some pop ups came out, don't worry I am handling it")
+            try:
+                browser.find_element_by_class_name("mfp-close").click()
+            except NoSuchElementException:
+                pass
+
+        if i == itern + 1:
+            print("Collecting data from last page")
+            get_jobs_from_page(j, browser)
+
+    sleep(2)
+
     if len(j) > 0:
+        print("Total jobs found in all pages : {} ". format(len(j)))
         print("Job list preparation successful")
+        file = open("C:\\Users\pprra\PycharmProjects\Formfill\output_files\\{}.txt".format(job_name),
+                    mode="w+")
+        for k, v in j.items():
+            file.write(str(k) + ' : ' + str(v) + '\n')
+
+        file.close()
     else:
         print("No Jobs found, please verify the search criteria")
-#   pprint.pprint(j)
+
 
 
 get_job_details(url, "Cloud Developer")
